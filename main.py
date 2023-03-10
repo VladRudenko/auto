5135e6e9472a0b7ddc6b36901bc5535f5fdf1618
Vladyslav <108194441+VladRudenko@users.noreply.github.com>
2023-03-10 03:30:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
 
-service_obj = Service('C:\chromedriver.exe')
+service_obj = Service()
 driver = webdriver.Chrome(service = service_obj)
 driver.maximize_window()
 driver.get('https://qastartup.net/')
